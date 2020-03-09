70d900a2e8554cb8e316147bd441d9630ee837bc
Hatem <hatem.deux@gmail.com>
2020-03-09 14:50:34 -0400

    schema and seeds sql created and tested. old example files removed

---
+++ db/seeds/seeds_02_app_list.sql
@@ -0,0 +1,13 @@
+INSERT INTO app_list(id, name, domain)
+VALUES
+(1, 'facebook', 'https://www.facebook.com/'),
+(2, 'twitter', 'https://twitter.com/'),
+(3, 'youtube', 'https://www.youtube.com/'),
+(4, 'instagram', 'https://www.instagram.com/'),
+(5, 'google', 'https://www.google.com'),
+(6, 'bamboo', 'https://www.bamboohr.com/'),
+(7, 'harvest', 'https://www.getharvest.com/'),
+(8, 'linkedin', 'https://www.linkedin.com/'),
+(9, 'snapchat', 'https://www.snapchat.com/'),
+(10, 'reddit', 'https://www.reddit.com/');
+
---
+++ db/seeds/seeds_05_shared_access.sql
@@ -0,0 +1,10 @@
+INSERT INTO shared_access (credential_id, user_id, can_edit)
+VALUES
+(1,2, true),
+(2,1, false),
+(2,4, true),
+(2,9, true),
+(9,8, false),
+(9,1, true),
+(9,3, false)
+;
