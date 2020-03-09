a227c500b451d1d340acf1f23b7ce1c8360783ad
Eric-Lombardo <ejlanim8or@gmail.com>
2020-03-09 17:06:08 +0000

    make all templates cohesive

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("signIn");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
