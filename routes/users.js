221943d272fba8c66011c763d7deca24b514f1c7
victor poirier <poiriervictor99@gmail.com>
2020-03-10 13:55:25 +0000

    changed file names to better reflect their purposes

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
-  router.get("/signin", (req, res) => {
-    res.render("signIn");
+  router.get("/login", (req, res) => {
+    res.render("login");
   });
-  router.get("/signup", (req, res) => {
-    res.render("signUp");
+  router.get("/register", (req, res) => {
+    res.render("register");
   });
   router.post("/new", (req, res) => {
     //implement the code to handle the post for creating a new user
