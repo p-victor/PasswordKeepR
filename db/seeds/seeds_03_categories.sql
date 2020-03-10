fa08fed7a18aedf086b60e0dd0842f5824586f9e
Eric-Lombardo <ejlanim8or@gmail.com>
2020-03-10 23:33:10 +0000

    createPassword form will update DB for category_id

--- db/seeds/seeds_03_categories.sql
+++ db/seeds/seeds_03_categories.sql
@@ -1,12 +1,7 @@
 
 INSERT INTO categories (id, name) VALUES
-(0, 'ungategorized'),
-(1, 'category_01'),
-(2, 'category_02'),
-(3, 'category_03'),
-(4, 'category_04'),
-(5, 'category_05'),
-(6, 'Social'),
-(7, 'Entertainment'),
-(8, 'work');
+(0, 'uncategorized'),
+(1, 'social'),
+(2, 'entertainment'),
+(3, 'work');
 
--- routes/passwords.js
+++ routes/passwords.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { generatePassword } = require("../public/scripts/generatePassword")
+const { stripUrlToDomain } = require("../public/scripts/stripUrlToDomain")
 
 module.exports = ({createAppCredential}) => {
   router.get("/new", (req, res) => {
@@ -17,24 +18,36 @@ module.exports = ({createAppCredential}) => {
 
     let userObj = {id: 1}
     let appObj = {id: 1}
-    let catObj = {id: 0}
 
     let uppercase;
     let spChar;
     let numbers;
-
     !req.body.uppercase ? uppercase = false : uppercase = true;
     !req.body.spChar ? spChar = false : spChar = true;
     !req.body.numbers ? numbers = false : numbers = true;
 
+    let category = {id: 0};
+    if (req.body.category === "uncategorized") {
+      category.id = 0
+    } else if (req.body.category === "social") {
+      category.id = 1
+    } else if (req.body.category === "entertainment") {
+      category.id = 2
+    } else if (req.body.category === "work") {
+      category.id = 3
+    }
 
+    // let appObj = {id: 0};
+    let domainName = stripUrlToDomain(req.body.website_url);
+    console.log("this is the domain ====>", domainName);
+    // wait for hatem to make the functions so that i can search domain exists in app_list
 
     let appCred = {
       username: req.body.username,
       password: generatePassword(uppercase, req.body.length, spChar, numbers)
     }
 
-    createAppCredential(userObj, appCred, appObj, catObj);
+    createAppCredential(userObj, appCred, appObj, category);
     res.redirect('./new');
   });
   router.post("/:pass/update", (req, res) => {
