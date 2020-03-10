74d76d3348adf5f25b3c6776c120a41e530ed1a0
Victor Poirier <poiriervictor99@gmail.com>
2020-03-10 09:58:29 -0400

    changed /user/new route to render createPassword.ejs

--- routes/passwords.js
+++ routes/passwords.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/new", (req, res) => {
-    res.render("view");
+    res.render("createPassword");
   });
   router.get("/:pass", (req, res) => {
     const templateVar = {passwordInfo: req.params.pass};
