f7de74de84613a8f06ebeb3c8f28a03aecba2d88
Hatem <hatem.deux@gmail.com>
2020-03-11 12:24:40 -0400

    database seeds contained a bug caused forcing IDs. now all IDs are removed from inserts to be added correctly (except intentionally table categories.id = 0 => ungategorized)

--- db/seeds/seeds_03_categories.sql
+++ db/seeds/seeds_03_categories.sql
@@ -1,12 +1,13 @@
 
 INSERT INTO categories (id, name) VALUES
-(0, 'ungategorized'),
-(1, 'category_01'),
-(2, 'category_02'),
-(3, 'category_03'),
-(4, 'category_04'),
-(5, 'category_05'),
-(6, 'Social'),
-(7, 'Entertainment'),
-(8, 'work');
+(0, 'ungategorized');
+INSERT INTO categories (name) VALUES
+('category_01'),
+('category_02'),
+('category_03'),
+('category_04'),
+('category_05'),
+('Social'),
+('Entertainment'),
+('work');
 
